1fe518786507b927556188eeeb31d4b7f38b82d3
Ritika Rawat <Ritikarawat220@gmail.com>
2023-10-13 15:53:41 +0530

    Add form validation

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
+
 function RegistrationForm() {
   const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    contactNumber: '',
+    bio: '',
+  });
+
+  const [errors, setErrors] = useState({
     name: '',
     email: '',
     contactNumber: '',
@@ -40,7 +48,42 @@ function RegistrationForm() {
   };
 
   const validateForm = () => {
-    return true; // You can add validation logic here if needed.
+    let isValid = true;
+    const updatedErrors = {
+      name: '',
+      email: '',
+      contactNumber: '',
+      bio: '',
+    };
+
+    // Validate Name
+    if (formData.name.trim() === '') {
+      updatedErrors.name = 'Full name is required';
+      isValid = false;
+    }
+
+    // Validate Email
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!emailRegex.test(formData.email)) {
+      updatedErrors.email = 'Invalid email address';
+      isValid = false;
+    }
+
+    // Validate Contact Number
+    const phoneNumberRegex = /^\d{10}$/; // Change this regex as needed
+    if (!phoneNumberRegex.test(formData.contactNumber)) {
+      updatedErrors.contactNumber = 'Invalid contact number';
+      isValid = false;
+    }
+
+    // Validate Bio
+    if (formData.bio.trim() === '') {
+      updatedErrors.bio = 'Bio is required';
+      isValid = false;
+    }
+
+    setErrors(updatedErrors);
+    return isValid;
   };
 
   return (
@@ -54,6 +97,8 @@ function RegistrationForm() {
           onChange={handleInputChange}
           required
         />
+        {errors.name && <span className="error">{errors.name}</span>}
+
         <input
           type="email"
           name="email"
@@ -62,6 +107,8 @@ function RegistrationForm() {
           onChange={handleInputChange}
           required
         />
+        {errors.email && <span className="error">{errors.email}</span>}
+
         <input
           type="tel"
           name="contactNumber"
@@ -70,6 +117,8 @@ function RegistrationForm() {
           onChange={handleInputChange}
           required
         />
+        {errors.contactNumber && <span className="error">{errors.contactNumber}</span>}
+
         <textarea
           name="bio"
           placeholder="Bio"
@@ -77,9 +126,12 @@ function RegistrationForm() {
           onChange={handleInputChange}
           required
         />
+        {errors.bio && <span className="error">{errors.bio}</span>}
+
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 }
+
 export default RegistrationForm;
